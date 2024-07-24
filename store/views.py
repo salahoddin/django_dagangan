374803be7127b67498a40dcb600872a439c9f5c7
salahoddin <salahoddin.sambutuan89@gmail.com>
2024-07-24 11:55:49 +0800

    add category view

--- store/views.py
+++ store/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from . models import Category
 
 # Create your views here.
 def store(request):
     return render(request, 'store/store.html')
+
+def categories(request):
+    categories = Category.objects.all()
+
+    return {'categories': categories}
