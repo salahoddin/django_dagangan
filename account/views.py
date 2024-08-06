503d7bbab4aca5d39de976f0c844185579be737e
salahoddin <salahoddin.sambutuan89@gmail.com>
2024-08-06 09:27:56 +0800

    configure registration form

--- account/views.py
+++ account/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . forms import CreateUserForm
+
+
 # Create your views here.
 def register(request):
-    return HttpResponse('account url setup')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save() 
+            return redirect('store')
+
+    return render(request, 'account/registration/register.html', {'form': form})
